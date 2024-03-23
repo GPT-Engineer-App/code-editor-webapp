415c8e0fdf54596682ae09c86d16e51ecb217619
Katrin <katrin.brueheim@gmx.de>
2024-03-23 14:05:49 +0100

    Mock files: Removed additional whitespace in md file.

--- src/mockFiles.js
+++ src/mockFiles.js
@@ -1,31 +1,5 @@
 const mockFiles = {
   "index.html": `<!DOCTYPE html>
-<<<<<<< HEAD
-    <html>
-      <head>
-        <title>My Web App</title>
-      </head>
-      <body>
-        <h1>Welcome to my web app!</h1>
-        <p>This is a simple web app.</p>
-        <script src="app.js"></script>
-      </body>
-    </html>`,
-  "app.js": `
-    console.log("Hello, World!");
-  `,
-  "styles/main.css": `
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-    }
-    
-    h1 {
-      color: #333;
-    }`,
-  "README.md": `# My Web App
-=======
 <html>
   <head>
     <title>My Web App</title>
@@ -46,9 +20,7 @@ const mockFiles = {
 h1 {
   color: #333;
 }`,
-  "README.md": `
-    # My Web App
->>>>>>> 4fdf22b9108f297d3cddcefcf26229ecd609b62d
+  "README.md": `# My Web App
     
     This is a simple web application.
     
